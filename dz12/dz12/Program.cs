10c6625fd3f48f2045e3f1b4c5cfe04df7dd06cf
AWPMasterGames <126659812+AWPMasterGames@users.noreply.github.com>
2024-10-14 18:37:42 +0300

    Update Program.cs

--- dz12/dz12/Program.cs
+++ dz12/dz12/Program.cs
@@ -1,56 +1,74 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace Homework_29_04
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void z1()
         {
-
-            //1 задача
-            /*Console.WriteLine("Ведите число: ");
-            int number = int.Parse(Console.ReadLine());
-
-            while (number < 0 || number > 100)
+            Console.WriteLine("Ведите число: ");
+            List<string> Message = new List<string>();
+            int number = 0;
+            try
             {
-                Console.WriteLine("!!ошибка!!\nведите число сново: ");
+                number = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                z1();
+            }
+            while (number < 1 || number > 99)
+            {
+                Console.WriteLine("!!Ошибка: Число должно быть больше 0 и меньше 100!!\nВедите число сново: ");
                 number = int.Parse(Console.ReadLine());
             }
 
-            if (number % 3 == 0 && number % 5 != 0) Console.Write("Fizz");
-            if (number % 5 == 0 && number % 3 != 0) Console.Write("Buzz");
-            if (number % 3 == 0 && number % 5 == 0) Console.Write("Fizz Buzz");
-            if (number % 3 != 0 && number % 5 != 0) Console.WriteLine(number);*/
 
 
-            //2 задача
-            /*Console.Write("Ведите число: ");
+            if (number % 3 == 0) Message.Add("Fizz");
+
+            if (number % 5 == 0) Message.Add("Buzz");
+
+            if (number % 3 != 0 && number % 5 != 0) Message.Add($"{number}");
+
+            foreach (string s in Message) Console.WriteLine(s);
+        }
+
+        static void z2()
+        {
+            Console.Write("Ведите число: ");
             float number = float.Parse(Console.ReadLine());
 
             Console.Write("введите процент: ");
             float percent = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Результат: " + (number * (percent / 100)));*/
+            Console.WriteLine($"Результат: {number * (percent / 100)}");
+        }
+        static void z3()
+        {
+            int[] Numbers = new int[4];
 
-            //3 задача
-            /*Console.Write("Введите первое число: ");
-            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите первое число: ");
+            Numbers[0] = int.Parse(Console.ReadLine());
 
-            Console.Write("введите второе число: ");
-            int number2 = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            Numbers[1] = int.Parse(Console.ReadLine());
 
-            Console.Write("введите третье число: ");
-            int number3 = int.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            Numbers[2] = int.Parse(Console.ReadLine());
 
-            Console.Write("введите четвёртое число: ");
-            int number4 = int.Parse(Console.ReadLine());
+            Console.Write("Введите четвёртое число: ");
+            Numbers[3] = int.Parse(Console.ReadLine());
 
-            int result = number1 * 1000 + number2 * 100 + number3 * 10 + number4;
+            int result = Numbers[0] * 1000 + Numbers[1] * 100 + Numbers[2] * 10 + Numbers[3];
 
-            Console.WriteLine("The result is " + result);*/
-
-            //4 задача
-            /*Console.Write("Введите число: ");
+            Console.WriteLine($"Результат: {result}");
+        }
+        static void z4()
+        {
+            Console.Write("Введите число: ");
             string number = Console.ReadLine();
             while (number.Length != 6)
             {
@@ -61,24 +79,25 @@ namespace Homework_29_04
             int pos1 = int.Parse(Console.ReadLine());
             Console.Write("Введите вторую позицию: ");
             int pos2 = int.Parse(Console.ReadLine());
-            for (int i = 0;i < number.Length; i++)
+            for (int i = 0; i < number.Length; i++)
             {
-                if (i == pos1-1)
+                if (i == pos1 - 1)
                 {
-                    Console.Write(number[pos2-1]);
+                    Console.Write(number[pos2 - 1]);
                 }
-                else if (i == pos2-1)
+                else if (i == pos2 - 1)
                 {
-                    Console.Write(number[pos1-1]);
+                    Console.Write(number[pos1 - 1]);
                 }
                 else
                 {
                     Console.Write(number[i]);
                 }
-            }*/
-
-            //7 задача
-            /*Console.WriteLine("Введите первую границу: ");
+            }
+        }
+        static void z7()
+        {
+            Console.WriteLine("Введите первую границу: ");
             int left = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите вторую границу: ");
             int right = int.Parse(Console.ReadLine());
@@ -93,7 +112,25 @@ namespace Homework_29_04
             for (int i = left; i <= right; i++)
             {
                 if (i % 2 == 0) Console.Write(i + " ");
-            }*/
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            //1 задача
+            //z1();
+
+            //2 задача
+            //z2();
+
+            //3 задача
+            //z3();
+
+            //4 задача
+            //z4();
+
+            //7 задача
+            //z7();
         }
     }
 }
