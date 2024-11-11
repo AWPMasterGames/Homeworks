b15c9c48990377f99bdd36b978a195c4b1f9396a
AWPMasterGames <126659812+AWPMasterGames@users.noreply.github.com>
2024-11-11 13:29:56 +0300

    reg

---
+++ Reg/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Reg
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    ///
+    public partial class MainWindow : Window
+    {
+        char[] symbols = "!\"#$%&'()*+,-./:;<=>?@[ \\ ]^_`{|}".ToCharArray();
+        List<User> Users = new List<User>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ConfirmBtn(object sender, RoutedEventArgs e)
+        {
+            if (CheckData()) Users.Add(new User(Username.Text, Email.Text, CPassword.Password, Birthday.Text));
+        }
+        private bool CheckData()
+        {
+            if (Username.Text.Length < 3)
+            {
+                UsernameInfo.Text = "Username: Имя пользователя должно быть не менее 3 символа";
+                return false;
+            }
+            UsernameInfo.Text = "Username";
+            if (IsUserExist())
+            {
+                UsernameInfo.Text = "Username: Пользователь уже существует";
+            }
+            if (Password.Password.Length < 6)
+            {
+                PasswordInfo.Text = "Password: Пароль должен быть длинее 5 символов";
+                return false;
+            }
+
+            if (!FindSymbols())
+            {
+                PasswordInfo.Text = "Password: Пароль должен содержвать в себе символы $#!@._-...";
+                return false;
+            }
+
+            if (!FindDigits())
+            {
+                PasswordInfo.Text = "Password: Пароль должен содержвать в себе цифры";
+
+                return false;
+            }
+            PasswordInfo.Text = "Password";
+
+            if (CPassword.Password != Password.Password)
+            {
+                CPasswordInfo.Text = "Confirm password: Пароли должны быть одинаковыми";
+                return false;
+            }
+            CPasswordInfo.Text = "Confirm password";
+
+            if (Birthday.Text.Length < 10)
+            {
+                DateInfoInfo.Text = "Birth date: выберите дату в календоре";
+                return false;
+            }
+            DateInfoInfo.Text = "Birth date";
+            return true;
+        }
+        private bool FindSymbols()
+        {
+            for (int i = 0; i < symbols.Length; i++)
+            {
+                if (Password.Password.IndexOf(symbols[i]) > -1) return true;
+            }
+            return false;
+        }
+        private bool FindDigits()
+        {
+            int count = 0;
+            for (int i = 0; i < Password.Password.Length; i++)
+            {
+                if (char.IsDigit(Password.Password[i])) { count++; }
+            }
+            if (count > 0) return true;
+            return false;
+        }
+
+        private void Password_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Password.Password.Length < 6)
+            {
+                PasswordInfo.Text = "Password: Пароль должен быть длинее 5 символов";
+            }
+
+
+            if (!FindSymbols())
+                PasswordInfo.Text = "Password: Пароль должен содержвать в себе символы $#!@._-...";
+
+            if (!FindDigits())
+                PasswordInfo.Text = "Password: Пароль должен содержвать в себе цифры";
+
+        }
+        private bool IsUserExist()
+        {
+            for (int i = 0; i < Users.Count; i++)
+            {
+                if (Users[i].Name == Username.Text)
+                {
+                    return true;
+                }
+                if (Users[i].Email == Email.Text)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Reg/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class User
+{
+    private string _Name;
+    private string _Email;
+    private string _Password;
+    private string _BirthDay;
+
+    public string Name { get { return _Name; } }
+    public string Email { get { return _Email; } }
+
+    public User() { }
+    public User(string name, string email, string password, string birthday)
+    {
+        this._Name = name;
+        this._Email = email;
+        this._Password = password;
+        this._BirthDay = birthday;
+    }
+}
+
